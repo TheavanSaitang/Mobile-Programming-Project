a5c7e4fe6bba481b8f78c0dc16e49d1f6056b746
Phillip-Drake <Drakester74@gmail.com>
2023-12-02 16:41:08 -0600

    *Complete* SteamAPI Integration

--- mPProject/app/src/main/java/edu/uark/ahnelson/mPProject/Model/GameRepository.kt
+++ mPProject/app/src/main/java/edu/uark/ahnelson/mPProject/Model/GameRepository.kt
@@ -3,10 +3,18 @@ package edu.uark.ahnelson.mPProject.Model
 import android.os.AsyncTask
 import android.util.Log
 import androidx.annotation.WorkerThread
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
+import okhttp3.Dispatcher
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okio.IOException
+import org.json.JSONArray
 import org.json.JSONObject
 
 // Declares the DAO as a private property in the constructor. Pass in the DAO
@@ -28,45 +36,59 @@ class GameRepository(private val gameDao: GameDao) {
     }
     private val client = OkHttpClient()
     private val APIKey = "FCBCDE0D333F3FA53CE2A1AB19FCCE52"
+
     //connects to Steam API, gets all app id's from a user with a specified userId
-    fun getSteamGames(userId:String){
+    suspend fun getSteamGames(userId: String) = withContext(Dispatchers.IO) {
 
-        doAsync {
-            Log.d("Test", "This is being called")
-            val request = Request.Builder()
-                //https://api.steampowered.com/ISteamApps/GetAppList/v2
-                .url("https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$APIKey&steamid=$userId&format=json")
-                .build()
-
-            client.newCall(request).execute().use { response ->
-                if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-                for ((name, value) in response.headers) {
-                    println("$name: $value")
-                }
-                val dataInJSON = JSONObject(response.body!!.string())
-                val games = dataInJSON.getJSONArray("games")
-
-                Log.d("DatabaseGame", dataInJSON.toString())
-                Log.d("Database", response.body!!.string())
+        val request = Request.Builder()
+            //https://api.steampowered.com/ISteamApps/GetAppList/v2
+            .url("https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$APIKey&steamid=$userId&format=json")
+            .build()
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+            for ((name, value) in response.headers) {
+                println("$name: $value")
             }
-        }.execute()
-    }
-    fun getSteamGame(appId:String){
-        doAsync {
-            val request = Request.Builder()
-                .url("https://api.steampowered.com/ICommunityService/GetApps/v1/?key=FCBCDE0D333F3FA53CE2A1AB19FCCE52&appids%5B0%5D=2247630")
-                .build()
-            client.newCall(request).execute().use {response ->
-                if(!response.isSuccessful) throw IOException("Unexpected code $response")
-                for((name, value) in response.headers) {
-                    println("$name: $value")
-                }
+            convertSteamGames(response.body!!.string())
 
+        }
+    }
+    private fun convertSteamGames(APIReturn: String){
+        val gamesArray = JSONObject(APIReturn)
+            .getJSONObject("response").getJSONArray("games")
+        //getSteamGameInfo(gamesArray.getJSONObject(0).getString("appid"))
+        for(i in 0 until gamesArray.length())
+        {
+            //Log.d("Database", gamesArray.getJSONObject(i).get("appid").toString())
+            Log.d("Debug", i.toString())
+            Log.d("Debug", gamesArray.length().toString())
+            getSteamGameInfo(gamesArray.getJSONObject(i).getString("appid"))
+        }
+        Log.d("Test", "Made it!")
+    }
+    fun getSteamGameInfo(appId:String) {
+        val request = Request.Builder()
+            .url("https://api.steampowered.com/ICommunityService/GetApps/v1/?key=FCBCDE0D333F3FA53CE2A1AB19FCCE52&appids%5B0%5D=$appId")
+            .build()
+        client.newCall(request).execute().use {response ->
+            if(!response.isSuccessful) throw IOException("Unexpected code $response")
+            for((name, value) in response.headers) {
+                println("$name: $value")
             }
-        }.execute()
+            parseGameInfo(response.body!!.string())
+        }
     }
-
+    fun parseGameInfo(APIReturn: String){
+        val objectJSON = JSONObject(APIReturn)
+            .getJSONObject("response").getJSONArray("apps").getJSONObject(0)
+        val name = objectJSON.getString("name")
+        val icon = objectJSON.getString("icon")
+        runBlocking{
+            launch{
+                insert(Game(null, name, false, 0, "PC", icon, "", "", 0, "", "", 0))
+            }
+        }
+    }
     // By default Room runs suspend queries off the main thread, therefore, we don't need to
     // implement anything else to ensure we're not doing long running database work
     // off the main thread.
